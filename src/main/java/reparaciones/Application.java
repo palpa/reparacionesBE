d1159c0dc8e1e3e5819d1ae4ad89a91d724a22b0
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-06-14 16:14:27 -0300

    chore(Application): remove unused import and commented annotation

--- src/main/java/reparaciones/Application.java
+++ src/main/java/reparaciones/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-//@EnableHypermediaSupport(type = {EnableHypermediaSupport.HypermediaType.HAL})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
